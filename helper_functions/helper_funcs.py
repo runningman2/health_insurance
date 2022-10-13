0fc8aa9f6055b78977be7bcca86a8b5f476435a9
runningman2 <kyle.rewick@gmail.com>
2022-10-13 17:12:24 -0500

    added plotly plots

--- helper_functions/helper_funcs.py
+++ helper_functions/helper_funcs.py
@@ -3,6 +3,7 @@ Helper functions
 """
 
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 def plot_plans(plan_objs, min_spend, max_spend, step_spend):
     """
@@ -13,11 +14,19 @@ def plot_plans(plan_objs, min_spend, max_spend, step_spend):
     """
     
     fig, ax = plt.subplots()
+    fig2 = go.Figure()
     for plan in plan_objs:
         costs, spend = plan.calc_annual_cost(min_spend, max_spend, step_spend)
         ax.plot(spend, costs, label=plan.name)
+        fig2.add_trace(go.Scatter(x=spend, y=costs, name=plan.name, mode='lines'))
+        
         
     ax.set_xlabel('Medical spend')
     ax.set_ylabel('Cost ($)')
     ax.set_title('Medical plan comparison')
     ax.legend()
+    
+    fig2.update_layout(title='Medical plan comparison', xaxis_title='Medical spend',
+                       yaxis_title='Cost ($)')
+    fig2.show()
+    
